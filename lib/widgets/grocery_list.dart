c231a8f479d166203924d704d7d0e995c55f42a5
HadilDrissi <hadil.drissi@ieee.org>
2023-12-09 15:11:35 +0100

    first commit

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeGrocery(GroceryItem groceryItem) {
+    final groceryItemIndex = _groceryItems.indexOf(groceryItem);
+    setState(() {
+      _groceryItems.remove(groceryItem);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(seconds: 3),
+      content: const Text('Item deleted.'),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          setState(() {
+            _groceryItems.insert(groceryItemIndex, groceryItem);
+          });
+        },
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    Widget mainContent = const Center(
+      child: Text('No Grocery found. Start adding some!'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      mainContent = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(_groceryItems[index]),
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+            margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).colorScheme.onBackground.opacity),
+          ),
+          onDismissed: (direction) {
+            _removeGrocery(_groceryItems[index]);
+          },
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
+      body: width < 600
+          ? Column(
+              children: [Expanded(child: mainContent)],
+            )
+          : Row(children: [Expanded(child: mainContent)]),
+    );
+  }
+}
